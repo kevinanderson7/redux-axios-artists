82c0f470060916101cf992f4c500cd5f31aacf97
Kevin Anderson <kmichael419@hotmail.com>
2020-07-23 14:14:51 -0500

    initial array of artists is being sent over to the artist reducer as it should upon page load

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,15 +4,17 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 import ArtistList from './../ArtistList/ArtistList.js';
+import { connect } from 'react-redux';
 
 class App extends Component {
   // Called when the (App) component is created
   state = {
     artists: [],
-  }
-  
+  };
+
   // DOM is ready
-  componentDidMount() { // react Component method
+  componentDidMount() {
+    // react Component method
     this.refreshArtists();
   }
 
@@ -20,15 +22,23 @@ class App extends Component {
     // just like $.ajax()
     axios({
       method: 'GET',
-      url: '/artist'
-    }).then((response) => {
-      console.log(response);
-      // response.data will be the array of artists
-      this.setState({
-        artists: response.data,
+      url: '/artist',
+    })
+      .then((response) => {
+        console.log(response);
+        // response.data will be the array of artists
+        // this.setState({
+        //   artists: response.data,
+        // });
+        this.props.dispatch({
+          type: 'SET_ARTISTS',
+          payload: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log('error: ', error);
       });
-    });
-  }
+  };
 
   render() {
     return (
@@ -36,11 +46,14 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Famous Artists</h1>
         </header>
-        <br/>
-        <ArtistList refreshArtists={this.refreshArtists} artistList={this.state.artists} />
+        <br />
+        <ArtistList
+          refreshArtists={this.refreshArtists}
+          artistList={this.state.artists}
+        />
       </div>
     );
   }
 }
 
-export default App;
+export default connect()(App);
