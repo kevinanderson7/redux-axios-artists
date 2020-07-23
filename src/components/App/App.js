1f1775bc0f749e91f0e3a39da151c673d96463dd
Kevin Anderson <kmichael419@hotmail.com>
2020-07-23 15:28:24 -0500

    setting up client side routes and navigation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,9 @@ import './App.css';
 import axios from 'axios';
 import ArtistList from './../ArtistList/ArtistList.js';
 import { connect } from 'react-redux';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import ArtistForm from './../ArtistForm/ArtistForm';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   // Called when the (App) component is created
@@ -42,16 +45,30 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Famous Artists</h1>
-        </header>
-        <br />
-        <ArtistList
-          refreshArtists={this.refreshArtists}
-          artistList={this.state.artists}
-        />
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <div>
+              <Link className="linkStyle" to="/addArtist">
+                Add Artist
+              </Link>
+            </div>
+            <div>
+              <Link className="linkStyle" to="/allArtists">
+                Artist List
+              </Link>
+            </div>
+            <h1 className="App-title">Famous Artists</h1>
+          </header>
+          <br />
+          <ArtistList
+            refreshArtists={this.refreshArtists}
+            artistList={this.state.artists}
+          />
+          <Route exact path="/addArtist" component={ArtistForm} />
+          <Route exact path="/allArtists" component={ArtistList} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/ArtistForm/ArtistForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
+
+class ArtistForm extends Component {
+  render() {
+    return <div>TEST FORM</div>;
+  }
+}
+
+export default connect()(ArtistForm);
