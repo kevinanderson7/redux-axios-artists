6acde05b6780c713c41bb38d70bc9619fff4e962
Kevin Anderson <kmichael419@hotmail.com>
2020-07-23 14:24:12 -0500

    mapped store to props on artist list in order to get the artists to show up on DOM

--- src/components/ArtistList/ArtistList.js
+++ src/components/ArtistList/ArtistList.js
@@ -2,29 +2,40 @@
 
 import React, { Component } from 'react';
 import ArtistListItem from '../ArtistListItem/ArtistListItem';
+import { connect } from 'react-redux';
 
 class ArtistList extends Component {
-    createArtistList() {
-        let artistListForDom = [];
-        for(let i = 0; i < this.props.artistList.length; i += 1) {
-            let artist = this.props.artistList[i];
-            let artistRow = (<ArtistListItem key={i} refreshArtists={this.props.refreshArtists} artist={artist} />);
-            artistListForDom.push(artistRow);
-        }
-        return artistListForDom;
+  createArtistList() {
+    let artistListForDom = [];
+    for (let i = 0; i < this.props.store.artistReducer.length; i += 1) {
+      let artist = this.props.store.artistReducer[i];
+      let artistRow = (
+        <ArtistListItem
+          key={i}
+          refreshArtists={this.props.refreshArtists}
+          artist={artist}
+        />
+      );
+      artistListForDom.push(artistRow);
     }
+    return artistListForDom;
+  }
 
-    render() {
-        return (
-            <div>
-                <table>
-                    <tbody>
-                        {this.createArtistList()}
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <table>
+          <tbody>{this.createArtistList()}</tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
-export default ArtistList;
+const mapStoreToProps = (store) => {
+  return {
+    store: store,
+  };
+};
+
+export default connect(mapStoreToProps)(ArtistList);
--- src/components/ArtistListItem/ArtistListItem.js
+++ src/components/ArtistListItem/ArtistListItem.js
@@ -2,24 +2,27 @@
 
 import React, { Component } from 'react';
 import axios from 'axios';
+import { connect } from 'react-redux';
 
 class ArtistListItem extends Component {
-    deleteArtist = () => {
-        axios({
-            method: 'DELETE',
-            url: `/artist/${this.props.artist.id}`
-        }).then((response) => {
-            this.props.refreshArtists();
-        });
-    }
-    render() {
-        return (
-            <tr>
-                <td>{this.props.artist.name}</td>
-                <td><button onClick={this.deleteArtist}>DELETE</button></td>
-            </tr>
-        );
-    }
+  deleteArtist = () => {
+    axios({
+      method: 'DELETE',
+      url: `/artist/${this.props.artist.id}`,
+    }).then((response) => {
+      this.props.refreshArtists();
+    });
+  };
+  render() {
+    return (
+      <tr>
+        <td>{this.props.artist.name}</td>
+        <td>
+          <button onClick={this.deleteArtist}>DELETE</button>
+        </td>
+      </tr>
+    );
+  }
 }
 
-export default ArtistListItem;
+export default connect()(ArtistListItem);
